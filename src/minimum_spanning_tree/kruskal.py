83832b03db58b207cfd805e80ef3efdc16919b8c
Wagner Silva <wagner.silva+ptek@practicetek.com>
2025-09-30 11:09:26 -0300

    improvements

--- src/minimum_spanning_tree/kruskal.py
+++ src/minimum_spanning_tree/kruskal.py
@@ -9,29 +9,30 @@ Edge = Tuple[Vertex, Vertex]
 Graph = Tuple[Set[Vertex], List[Edge]]
 WeightFunction = Dict[Edge, float]
 
-def find_set(vertex_set: Dict[Vertex, Tuple[Vertex, int]], v: Vertex) -> Tuple[Vertex, int]:
-    if vertex_set[v][0] == v:
+def find_set(vertex_set: Dict[Vertex, Vertex], v: Vertex) -> Vertex:
+    if vertex_set[v] == v:
         return vertex_set[v]
-    return find_set(vertex_set, vertex_set[v][0])
+    return find_set(vertex_set, vertex_set[v])
 
 def kruskal(g: Graph, w: WeightFunction) -> List[Edge]:
     A = []
     if not g[1]:
         return A
-    vertex_set = {v: (v, 0) for v in g[0]}
+    vertex_set = {v: v for v in g[0]}
+    rank = {v: 0 for v in g[0]}
     sorted_edges = sorted(g[1], key=lambda edge: w[edge])
     for edge in sorted_edges:
         u, v = edge
-        parentU, rankU = find_set(vertex_set, u)
-        parentV, rankV = find_set(vertex_set, v)
-        if parentU != parentV:
-            if rankU < rankV:
-                vertex_set[parentU] = (parentV, rankU)
-            elif rankU > rankV:
-                vertex_set[parentV] = (parentU, rankV)
+        rootU = find_set(vertex_set, u)
+        rootV = find_set(vertex_set, v)
+        if rootU != rootV:
+            if rank[rootU] < rank[rootV]:
+                vertex_set[rootU] = rootV
+            elif rank[rootU] > rank[rootV]:
+                vertex_set[rootV] = rootU
             else:
-                vertex_set[parentV] = (parentU, rankV)
-                vertex_set[parentU] = (parentU, rankU + 1)
+                vertex_set[rootV] = rootU
+                rank[rootU] += 1
             A.append(edge)
             if len(A) == len(g[0]) - 1:
                 break
